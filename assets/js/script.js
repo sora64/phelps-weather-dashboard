0e6937539358472dc8c7d9d2ca6ca3bf23b08573
Alan Phelps <phelpsa64@gmail.com>
2022-01-04 18:27:31 -0500

    still working on localStorage issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,47 @@
 const citySearchInputEl = document.querySelector('#cityname');
 const cityFormEl = document.querySelector('#cityForm');
+const citiesSearchedContainerEl = document.querySelector('#citiesSearchedContainer');
+
+let citiesArray = [];
+let weatherArray = [];
 
 let getCityWeather = function(city) {
-    // formate the OpenWeather One Call api url
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=26e14b1e1bdcd3e4a900e722776adf30";
+    // formate the OpenWeather api url
+    let apiUrlOne = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=26e14b1e1bdcd3e4a900e722776adf30";
 
-    // make a request to the url
-    fetch(apiUrl)
+    // make a request to the first url
+    fetch(apiUrlOne)
     .then(function(response) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                localStorage.setItem(JSON.stringify(data.name), JSON.stringify(data));
-                displayCities();
+                let cityLat = data.coord.lat;
+                // console.log(cityLat);
+                let cityLon = data.coord.lon;
+                // console.log(cityLon);
+                citiesArray.push(data.name);
+                // console.log(citiesArray);
+                localStorage.setItem("cities", JSON.stringify(citiesArray));
+
+                let apiUrlTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude=minutely,hourly&appid=26e14b1e1bdcd3e4a900e722776adf30"
+
+                // make a request to the second url
+                fetch(apiUrlTwo)
+                .then(function(response) {
+                    if (response.ok) {
+                        response.json().then(function(data) {
+                            weatherArray.push({"weather": data});
+                            // console.log(weatherArray);
+                            localStorage.setItem("weather", JSON.stringify(weatherArray));
+                        })
+                    } else {
+                        alert('Error: City Not Found!');
+                    }
+                })
             })
+            displayCities();
         } else {
-            alert('Error: City Not Found!')
+            alert ('Error: City Not Found!')
         }
     })
     .catch(function(error) {
@@ -30,7 +56,6 @@ let formSubmitHandler = function(event) {
 
     if (cityName) {
         getCityWeather(cityName);
-        citySearchInputEl.value = '';
     } else {
         alert('Please enter a valid City.');
     }
@@ -38,17 +63,10 @@ let formSubmitHandler = function(event) {
 
 cityFormEl.addEventListener('submit', formSubmitHandler);
 
-let displayCities = function(i) {
-    for (i = 0; i < localStorage.length; i++) {
-        let cityArray = JSON.parse(localStorage.getItem(localStorage.key(i))).name;
-        
-        let currentIndex = localStorage.length-=1;
-
-        console.log(currentIndex);
-
-        let currentCity = cityArray.slice(currentIndex);
-
-        console.log(currentCity);
+let displayCities = function() {
+    let citiesSearched = JSON.parse(localStorage.getItem("cities"));
+    for (let i = 0; i < citiesSearched.length; i++) {
+        console.log(citiesSearched[i]);
     }
 };
 
