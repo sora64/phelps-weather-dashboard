96eb6402444e2a5ae3571737e405c57eb8fb5463
Alan Phelps <phelpsa64@gmail.com>
2022-01-06 21:32:11 -0500

    the temperature, wind speed, humidity, and UVI (which also changes color to show severity) are now displayable on the page after searching for a new city or clicking the button of an old one

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ const citySearchInputEl = document.querySelector('#cityname');
 const cityFormEl = document.querySelector('#cityForm');
 const mostRecentSearchContainerEL = document.querySelector('#mostRecentSearchContainer');
 const citiesSearchedContainerEl = document.querySelector('#citiesSearchedContainer');
-const currentWeatherEl = document.querySelector('#currentWeather');
+
+// current weather element variables
 const currentWeatherCityEL = document.querySelector('#currentWeatherCityEl');
 const currentWeatherIconEl = document.querySelector('#currentWeatherIconEl');
 const currentTemperatureEl = document.querySelector('#currentTempEl');
@@ -11,10 +12,20 @@ const currentWindSpeedEl = document.querySelector('#currentWindSpeedEl');
 const currentHumidityEl = document.querySelector('#currentHumidityEl');
 const currentUVIEl = document.querySelector('#currentUVI');
 const currentUVIValueEL = document.querySelector('#uvi-value');
+
+// tomorrow's weather element variables
 const tomorrowEl = document.querySelector('#tomorrowEl');
+
+// the day after tomorrow's variables
 const theNextDayEl = document.querySelector('#theNextDayEl');
+
+// the day after the day after tomorrow's variables
 const theNextNextDayEl = document.querySelector('#theNextNextDayEl');
+
+// the day after the day after the day after tomorrow's variables
 const theNextNextNextDayEl = document.querySelector('#theNextNextNextDayEl');
+
+// the last forecasted day's variables
 const theFinalDayEl = document.querySelector('#theFinalDayEl');
 
 
@@ -39,7 +50,7 @@ let searchedCities = function() {
 
         cityButtonEl.addEventListener('click', searchedCurrentWeather);
     }
-};
+}
 
 searchedCities();
 
@@ -82,7 +93,6 @@ let getCityWeather = function(city) {
                     if (response.ok) {
                         response.json().then(function(data) {
                             displayCurrentWeather(data);
-
                             displayForecast(data);
                             
                         })
@@ -95,7 +105,7 @@ let getCityWeather = function(city) {
             alert ('Error: City Not Found!')
         }
     })
-};
+}
 
 let formSubmitHandler = function(event) {
     event.preventDefault();
@@ -111,9 +121,7 @@ let formSubmitHandler = function(event) {
     } else {
         alert('Please enter a valid City.');
     }
-};
-
-cityFormEl.addEventListener('submit', formSubmitHandler); 
+}
 
 function displayCurrentWeather(data) {
     weatherArray.push({ 'weather': data });
@@ -131,11 +139,11 @@ function displayCurrentWeather(data) {
         currentUVIValueEL.classList.remove('bg-warning');
         currentUVIValueEL.classList.remove('bg-danger');
         currentUVIValueEL.classList.add('bg-success');
-    } else if (currentUVI >= 3 && currentUVI <= 7) {
+    } else if (currentUVI > 2 && currentUVI < 7) {
         currentUVIValueEL.classList.remove('bg-danger');
         currentUVIValueEL.classList.remove('bg-success');
         currentUVIValueEL.classList.add('bg-warning');
-    } else if (currentUVI > 7) {
+    } else if (currentUVI >= 7) {
         currentUVIValueEL.classList.remove('bg-warning');
         currentUVIValueEL.classList.remove('bg-success');
         currentUVIValueEL.classList.add('bg-danger');
@@ -149,27 +157,177 @@ function displayForecast(data) {
     futureWeatherArray.push({ 'weather': data });
     let futureConditions = futureWeatherArray.pop().weather.daily;
     forecastedTemperatures(futureConditions);
+    forecastedWindSpeeds(futureConditions);
+    forecastedHumidity(futureConditions);
+    forecastedUVI(futureConditions);
 }
 
 function forecastedTemperatures(futureConditions) {
     let tommorrowTemp = futureConditions[0].temp.day;
-    let tomorrowTempValue = document.createElement('p');
-    tomorrowTempValue.textContent = "Temp: " + tommorrowTemp;
-    tomorrowEl.appendChild(tomorrowTempValue);
+    const tomorrowTempValue = document.getElementById('tomorrowTempEl');
+    tomorrowTempValue.textContent = 'Temp: ' + tommorrowTemp + '° F';
     let theNextDayTemp = futureConditions[1].temp.day;
-    let theNextDayTempValue = document.createElement('p');
-    theNextDayTempValue.textContent = "Temp: " + theNextDayTemp;
-    theNextDayEl.appendChild(theNextDayTempValue);    
+    const theNextDayTempValue = document.getElementById('theNextDayTempEl');
+    theNextDayTempValue.textContent = 'Temp: ' + theNextDayTemp + '° F';
     let theNextNextDayTemp = futureConditions[2].temp.day;
-    let theNextNextDayTempValue = document.createElement('p');
-    theNextNextDayTempValue.textContent = "Temp: " + theNextNextDayTemp;
-    theNextNextDayEl.appendChild(theNextNextDayTempValue);     
+    const theNextNextDayTempValue = document.getElementById('theNextNextDayTempEl');
+    theNextNextDayTempValue.textContent = 'Temp: ' + theNextNextDayTemp + '° F';
     let theNextNextNextDayTemp = futureConditions[3].temp.day;
-    let theNextNextNextDayTempValue = document.createElement('p');
-    theNextNextNextDayTempValue.textContent = "Temp: " + theNextNextNextDayTemp;
-    theNextNextNextDayEl.appendChild(theNextNextNextDayTempValue);     
+    const theNextNextNextDayTempValue = document.getElementById('theNextNextNextDayTempEl');
+    theNextNextNextDayTempValue.textContent = 'Temp: ' + theNextNextNextDayTemp + '° F';
     let theFinalDayTemp = futureConditions[4].temp.day;
-    let theFinalDayTempValue = document.createElement('p');
-    theFinalDayTempValue.textContent = "Temp: " + theFinalDayTemp;
-    theFinalDayEl.appendChild(theFinalDayTempValue);
+    const theFinalDayTempValue = document.getElementById('theFinalDayTempEl');
+    theFinalDayTempValue.textContent = 'Temp: ' + theFinalDayTemp + '° F';
 }
+
+function forecastedWindSpeeds(futureConditions) {
+    let tommorrowWindSpeed = futureConditions[0].wind_speed;
+    const tomorrowWindSpeedValue = document.getElementById('tomorrowWindSpeedEl');
+    tomorrowWindSpeedValue.textContent = 'Wind Speed: ' + tommorrowWindSpeed + ' MPH';
+    let theNextDayWindSpeed = futureConditions[1].wind_speed;
+    const theNextDayWindSpeedValue = document.getElementById('theNextDayWindSpeedEl');
+    theNextDayWindSpeedValue.textContent = 'Wind Speed: ' + theNextDayWindSpeed+ ' MPH';
+    let theNextNextDayWindSpeed = futureConditions[2].wind_speed;
+    const theNextNextDayWindSpeedValue = document.getElementById('theNextNextDayWindSpeedEl');
+    theNextNextDayWindSpeedValue.textContent = 'Wind Speed: ' + theNextNextDayWindSpeed+ ' MPH';
+    let theNextNextNextDayWindSpeed = futureConditions[3].wind_speed;
+    const theNextNextNextDayWindSpeedValue = document.getElementById('theNextNextNextDayWindSpeedEl');
+    theNextNextNextDayWindSpeedValue.textContent = 'Wind Speed: ' + theNextNextNextDayWindSpeed+ ' MPH';
+    let theFinalDayWindSpeed = futureConditions[4].wind_speed;
+    const theFinalDayWindSpeedValue = document.getElementById('theFinalDayWindSpeedEl');
+    theFinalDayWindSpeedValue.textContent = 'Wind Speed: ' + theFinalDayWindSpeed+ ' MPH';
+}
+
+function forecastedHumidity(futureConditions) {
+    let tomorrowHumidity = futureConditions[0].humidity;
+    const tomorrowHumidityValue = document.getElementById('tomorrowHumidityEl');
+    tomorrowHumidityValue.textContent = 'Humidity: ' + tomorrowHumidity + '%';
+    let theNextDayHumidity = futureConditions[1].humidity;
+    const theNextDayHumidityValue = document.getElementById('theNextDayHumidityEl');
+    theNextDayHumidityValue.textContent = 'Humidity: ' + theNextDayHumidity + '%';
+    let theNextNextDayHumidity = futureConditions[2].humidity;
+    const theNextNextDayHumidityValue = document.getElementById('theNextNextDayHumidityEl');
+    theNextNextDayHumidityValue.textContent = 'Humidity: ' + theNextNextDayHumidity + '%';
+    let theNextNextNextDayHumidity = futureConditions[3].humidity;
+    const theNextNextNextDayHumidityValue = document.getElementById('theNextNextNextDayHumidityEl');
+    theNextNextNextDayHumidityValue.textContent = 'Humidity: ' + theNextNextNextDayHumidity + '%';
+    let theFinalDayHumidity = futureConditions[4].humidity;
+    const theFinalDayHumidityValue = document.getElementById('theFinalDayHumidityEl');
+    theFinalDayHumidityValue.textContent = 'Humidity: ' + theFinalDayHumidity + '%';
+}
+
+function forecastedUVI(futureConditions) {
+    const uviTomorrowText = document.getElementById('tomorrowUVI');
+    uviTomorrowText.textContent = 'UVI: ';
+    const uviTheNextDayText = document.getElementById('theNextDayUVI');
+    uviTheNextDayText.textContent = 'UVI: ';
+    const uviTheNextNextDayText = document.getElementById('theNextNextDayUVI');
+    uviTheNextNextDayText.textContent = 'UVI: ';
+    const uviTheNextNextNextDayText = document.getElementById('theNextNextNextDayUVI');
+    uviTheNextNextNextDayText.textContent = 'UVI: ';
+    const uviTheFinalDayText = document.getElementById('theFinalDayUVI');
+    uviTheFinalDayText.textContent = 'UVI: ';
+
+    getTomorrowUVI();  
+    getTheNextDayUVI();
+    getTheNextNextDayUVI();
+    getTheNextNextNextDayUVI();
+    getTheFinalDayUVI();
+
+    function getTomorrowUVI() {
+        let tomorrowUVI = futureConditions[0].uvi;
+        const tomorrowUVIValueEl = document.getElementById('uvi-value-two');
+        tomorrowUVIValueEl.textContent = tomorrowUVI;
+        if (tomorrowUVI <= 2) {
+            tomorrowUVIValueEl.classList.remove('bg-warning');
+            tomorrowUVIValueEl.classList.remove('bg-danger');
+            tomorrowUVIValueEl.classList.add('bg-success');
+        } else if (tomorrowUVI > 2 && tomorrowUVI < 7) {
+            tomorrowUVIValueEl.classList.remove('bg-danger');
+            tomorrowUVIValueEl.classList.remove('bg-success');
+            tomorrowUVIValueEl.classList.add('bg-warning');
+        } else if (tomorrowUVI >= 7) {
+            tomorrowUVIValueEl.classList.remove('bg-warning');
+            tomorrowUVIValueEl.classList.remove('bg-success');
+            tomorrowUVIValueEl.classList.add('bg-danger');
+        };
+    }
+
+    function getTheNextDayUVI() {
+        let theNextDayUVI = futureConditions[1].uvi;
+        const theNextDayUVIValueEl = document.getElementById('uvi-value-three');
+        theNextDayUVIValueEl.textContent = theNextDayUVI;
+        if (theNextDayUVI <= 2) {
+            theNextDayUVIValueEl.classList.remove('bg-warning');
+            theNextDayUVIValueEl.classList.remove('bg-danger');
+            theNextDayUVIValueEl.classList.add('bg-success');
+        } else if (theNextDayUVI > 2 && theNextDayUVI < 7) {
+            theNextDayUVIValueEl.classList.remove('bg-danger');
+            theNextDayUVIValueEl.classList.remove('bg-success');
+            theNextDayUVIValueEl.classList.add('bg-warning');
+        } else if (theNextDayUVI >= 7) {
+            theNextDayUVIValueEl.classList.remove('bg-warning');
+            theNextDayUVIValueEl.classList.remove('bg-success');
+            theNextDayUVIValueEl.classList.add('bg-danger');
+        };
+    }
+
+    function getTheNextNextDayUVI() {
+        let theNextNextDayUVI = futureConditions[2].uvi;
+        const theNextNextDayUVIValueEl = document.getElementById('uvi-value-four');
+        theNextNextDayUVIValueEl.textContent = theNextNextDayUVI;
+        if (theNextNextDayUVI <= 2) {
+            theNextNextDayUVIValueEl.classList.remove('bg-warning');
+            theNextNextDayUVIValueEl.classList.remove('bg-danger');
+            theNextNextDayUVIValueEl.classList.add('bg-success');
+        } else if (theNextNextDayUVI > 2 && theNextNextDayUVI < 7) {
+            theNextNextDayUVIValueEl.classList.remove('bg-danger');
+            theNextNextDayUVIValueEl.classList.remove('bg-success');
+            theNextNextDayUVIValueEl.classList.add('bg-warning');
+        } else if (theNextNextDayUVI >= 7) {
+            theNextNextDayUVIValueEl.classList.remove('bg-warning');
+            theNextNextDayUVIValueEl.classList.remove('bg-success');
+            theNextNextDayUVIValueEl.classList.add('bg-danger');
+        };
+    }
+
+    function getTheNextNextNextDayUVI() {
+        let theNextNextNextDayUVI = futureConditions[3].uvi;
+        const theNextNextNextDayUVIValueEl = document.getElementById('uvi-value-five');
+        theNextNextNextDayUVIValueEl.textContent = theNextNextNextDayUVI;
+        if (theNextNextNextDayUVI <= 2) {
+            theNextNextNextDayUVIValueEl.classList.remove('bg-warning');
+            theNextNextNextDayUVIValueEl.classList.remove('bg-danger');
+            theNextNextNextDayUVIValueEl.classList.add('bg-success');
+        } else if (theNextNextNextDayUVI > 2 && theNextNextNextDayUVI < 7) {
+            theNextNextNextDayUVIValueEl.classList.remove('bg-danger');
+            theNextNextNextDayUVIValueEl.classList.remove('bg-success');
+            theNextNextNextDayUVIValueEl.classList.add('bg-warning');
+        } else if (theNextNextNextDayUVI >= 7) {
+            theNextNextNextDayUVIValueEl.classList.remove('bg-warning');
+            theNextNextNextDayUVIValueEl.classList.remove('bg-success');
+            theNextNextNextDayUVIValueEl.classList.add('bg-danger');
+        };
+    }
+
+    function getTheFinalDayUVI() {
+        let theFinalDayUVI = futureConditions[4].uvi;
+        const theFinalDayUVIValueEl = document.getElementById('uvi-value-six');
+        theFinalDayUVIValueEl.textContent = theFinalDayUVI;
+        if (theFinalDayUVI <= 2) {
+            theFinalDayUVIValueEl.classList.remove('bg-warning');
+            theFinalDayUVIValueEl.classList.remove('bg-danger');
+            theFinalDayUVIValueEl.classList.add('bg-success');
+        } else if (theFinalDayUVI > 2 && theFinalDayUVI < 7) {
+            theFinalDayUVIValueEl.classList.remove('bg-danger');
+            theFinalDayUVIValueEl.classList.remove('bg-success');
+            theFinalDayUVIValueEl.classList.add('bg-warning');
+        } else if (theFinalDayUVI >= 7) {
+            theFinalDayUVIValueEl.classList.remove('bg-warning');
+            theFinalDayUVIValueEl.classList.remove('bg-success');
+            theFinalDayUVIValueEl.classList.add('bg-danger');
+        };
+    }
+}
+
+cityFormEl.addEventListener('submit', formSubmitHandler);   
